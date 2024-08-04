555f9fd5933d75d0d5af81f838e28974756fd6b6
AncientDefender <kelvin.wong@live.ca>
2024-08-04 15:29:20 -0400

    Update combine_colors.py
    Removed 'lgt' light text style since a lot of modern terminals no longer support the faint text formatting.
    
    Included an option to combine with colored backgrounds,
    
    Added instructions on how to properly import this module with other Python scripts.

--- combine_colors.py
+++ combine_colors.py
@@ -1,131 +1,178 @@
 class Colors_Remix:
       """
-      A class to manage color codes by combining shades and colors.
+      A class to manage color codes by combining styles and colors.
+      In the scripts where you want to use the'Colors_Remix' class,
+
+      (1)  Include the following statement at the top:
+
+      >>>    from combine_colors import Colors_Remix
+
+      (2)  Create dictionaries for text styling and color codes:
+
+      >>>    styles = {
+      >>>        'reg': '\033[0m',  # Regular
+      >>>        'drk': '\033[1m',  # Dark
+      >>>        'itl': '\033[3m',  # Italic
+      >>>    }
+      >>>    colors = {
+      >>>        'blk': '\033[30m',  # Black
+      >>>        'red': '\033[31m',  # Red
+      >>>        'gre': '\033[32m',  # Green
+      >>>        'mos': '\033[33m',  # Moss
+      >>>        'blu': '\033[34m',  # Blue
+      >>>        'prp': '\033[35m',  # Purple
+      >>>    }
+
+      (3)  Create a dictionary of all possible style and color combinations:
+
+      >>>    combinations = Colors_Remix.create_combo(styles, colors, False)
+      >>>    combinations.update(Colors_Remix.create_combo(styles, colors, True))
+
+      (4)  Define an object for resetting special formatting to default:
+
+      >>>    reg = Colors_Remix(styles['reg'], styles, colors)
+
+      (5)  Make each combination available as a global variable by iterating through 'combinations':
+
+      >>>    for combo_name in combinations.keys():
+      >>>        globals()[combo_name] = combinations[combo_name]
+
+      For an example on implementing steps (2) to (5) in your scripts, see lines 141 to 154 of this module. It
+      will demonstrate how you can integrate this 'Colors_Remix' class to generate simplified formatted output.
       """
 
 
-      def __init__(his   ,   code   ,   shades   ,   colors):
+      def __init__(his   ,   code: str   ,   styles: str   ,   colors: str):
             his.decode   =   code
-            his.shades   =   shades
+            his.styles   =   styles
             his.colors   =   colors
 
 
       def __add__ (  his  ,  her  ):
-            if       not     isinstance(her  ,  Colors_Remix):
+            if  not  isinstance(her , Colors_Remix):
                      return  NotImplemented
 
-            if       not(    his.decode.startswith('\033[')  and  her.decode.startswith('\033[')    ):
-                     return  NotImplemented  # Ensure both objects have a common prefix \033[
+            if  not( his.decode.startswith( '\033[' )   and   her.decode.startswith( '\033[' ) ):
+                     return  NotImplemented  # Ensure both objects have a common prefix '\033['
 
             # Extract the numeric parts from both codes
-            his_digits   =   ''.join(filter(str.isdigit   ,   his.decode))
-            her_digit    =   ''.join(filter(str.isdigit   ,   her.decode))
+            his_digits   =   ''.join( filter( str.isdigit  ,  his.decode ) )
+            her_digit    =   ''.join( filter( str.isdigit  ,  her.decode ) )
             mutatation   =   f'{      his_digits      };{     her_digit     }'  # Combine the numeric parts with a semicolon separator
 
-            return   Colors_Remix(f'\033[{ mutatation }m'  ,  his.shades  ,  his.colors)  # Return a new formatting object in its combined color code
+            return   Colors_Remix(    f'\033[{mutatation}m' , his.styles , his.colors    )  # Returns a newly formattied object as a combined color code
 
 
       def __repr__(  his  ):
-            return ( f"ColorsRemix(code = {his.decode!r} , "
-                     f"shades = {his.shades!r} ,           "
-                     f"colors = {his.colors!r})            " )
+            return ( f"ColorsRemix(code = {his.decode!r} , styles = {his.styles!r} , colors = {his.colors!r})" )
 
 
       def __str__(   his   ):
             return   his.decode
 
 
-      def   get_code(his , shade , color):
-            shade_code  =  his.shades.get(shade , '')
-            color_code  =  his.colors.get(color , '')
-
-            return   shade_code  +  color_code
-
-
       @classmethod
-      def   create_combo(cls  ,  shades     ,    colors  ):
+      def   create_combo(  cls  ,  styling: str  ,  colors: str  ,  highlight: bool  )  ->  dict:
             """
-            Creates and returns every possible shade and color combination.Three
-            combinable 'shades' and six 'colors' allow for 18 unique concoctions.
+            Creates and returns every possible styling & color combinations. Three
+            combinable 'styling' with six 'colors' allow for 18 unique concoctions.
 
             Args:
-               shades (dict):          Dictionary of shade names and their codes.
-               colors (dict):          Dictionary of color names and their codes.
+               styling (dict):           Dictionary of style names and their codes.
+               colors (dict):            Dictionary of color names and their codes.
 
             Returns:
-               dict: Dictionary of combined names and their corresponding colors.
+               dict:     The updated dictionary of combinations and their mixtures.
             """
             combinations = {   }
-            for      shade_names , shade_syntax    in    shades.items(   ):
-                     for   color_names , color_syntax in colors.items(   ):
-                           if      shade_names  ==  'end':
-                                   continue  # Skip 'end' as it doesn't combine
-                           combo_name =  f"{ shade_names }{ color_names }"
-                           shade_obj  =  cls(shade_syntax , shades , colors)
-                           color_obj  =  cls(color_syntax , shades , colors)
-                           mutations  =  shade_obj  +  color_obj
-                           combinations[ combo_name ]   =   mutations
 
-                           globals(   )[ combo_name ]   =   mutations  # Dynamically create global variables for each combination
+            for      style_names , style_syntax    in      styling.items(   ):
+                     for   color_names , color_syntax  in   colors.items(   ):
+                           if    highlight:
+                                 color_names  =  color_names  +  'bg'
+                                 color_syntax =  color_syntax.replace('[3'  ,  '[4')
+                           combo_name  =  f"{    style_names    }{    color_names    }"
+                           style_obj   =  cls(   style_syntax  ,  styling  ,  colors   )
+                           color_obj   =  cls(   color_syntax  ,  styling  ,  colors   )
+                           mutations   =  style_obj    +    color_obj
+                           combinations[  combo_name  ]  =  mutations
 
-            return   combinations
+            return         combinations
 
 
       @staticmethod
-      def   cleanup_combo( combinations ):
+      def   cleanup_combo( combinations: dict )  ->  None:
             """
             Cleanup the dynamically created global variables for combinations.
     
             Args:
                combinations (dict): Dictionary of combination names to remove.
             """
-            for  name in   combinations:
-                 if   name in   globals(   ):
-                      del  globals(   )[name]
+            for      name  in      combinations:
+                     if    name    in    globals(   ):
+                           del     globals(   )[name]
 
 
       @staticmethod
-      def   show_example(  shades  ,  colors  ):
+      def   show_example(  colors: str  ,  styling: str  ,  highlight: bool  )  ->  None:
             """
-            Explains how shades and colors fuse together to create a combination.
-            This method will demonstrate each possible combination of shades and
-            colors to produce the final product: formatted text prints to screen.
+            Explains how styling and colors fuse together to create a combination.
+            This method will demonstrate each possible combination of styling and
+            colors to produce and display all formatted colored text in terminals.
 
             Args:
-               shades (dict):          Dictionary of shade names and their codes.
-               colors (dict):          Dictionary of color names and their codes.
+               colors (dict):           Dictionary of color names and their codes.
+               styling (dict):          Dictionary of style names and their codes.
             """
-            combinations = Colors_Remix.create_combo(shades , colors)
-    
-            for    combo_name , combo_obj   in   combinations.items(   ):
-                   print(  f"  {combo_obj}This line of text is written in script as '{ '{' }"
-                           f"{  combo_name  }{ '}' }'{ shades['end'] } using an f-string.   "  )
+            combinations = Colors_Remix.create_combo(styling , colors , highlight)
 
-            Colors_Remix.cleanup_combo( combinations )  # Clean up global variables
+            for    combo_name , combo_obj  in  combinations.items(   ):
+                   if      not  highlight:
+                           print(f"  {  combo_obj  }This line of text is written in script as '{'{'}"
+                                 f"{    combo_name    }{'}'}'{styling['reg']}    using an f-string. ")
+                   else:
+                           print(f"  {  combo_obj  }This line of text is written in script as '{'{'}"
+                                 f"{    combo_name    }{'}'}'{styling['reg']}  using an f-string.   ")
+
+            Colors_Remix.cleanup_combo( combinations )  # Cleaning up global variables
 
 
-shades   =   { 'end' : '\033[0m' ,  # Define shades
-               'drk' : '\033[1m' ,
-               'lgt' : '\033[2m' ,
-               'itl' : '\033[3m' , }
-colors   =   { 'blk' : '\033[30m',  # Define colors
-               'red' : '\033[31m',
-               'gre' : '\033[32m',
-               'brw' : '\033[33m',
-               'blu' : '\033[34m',
-               'prp' : '\033[35m', }
+# Define 'styles' and 'colors' dictionary
+styles  =  { 'reg'  :  '\033[0m'  ,  # Some modern terminals no longer support faint text style ('\033[2m') i.e., like the
+             'drk'  :  '\033[1m'  ,  # terminal I'm currently on — it's missing both 'dim' and 'inverse' style ('\033[7m')
+             'itl'  :  '\033[3m'  , }
+colors  =  { 'red'  :  '\033[31m' ,
+             'gre'  :  '\033[32m' ,
+             'mos'  :  '\033[33m' ,
+             'blu'  :  '\033[34m' ,
+             'prp'  :  '\033[35m' ,
+             'blk'  :  '\033[37m' , }
+combinations        =    Colors_Remix.create_combo(styles  ,  colors  ,  False)  # Create combinations without any highlighting
+reg                 =    Colors_Remix(   styles['reg']  ,  styles  ,  colors   )  # Define a variable to reset special formatting
+combinations.update(     Colors_Remix.create_combo( styles , colors , True )     )  # Include combinations with highlighted backgrounds
+for   combo_name    in   combinations.keys(   ):
+      globals(   )[      combo_name      ]  =  combinations[ combo_name ]  # Dynamically create global variables for all possible combinations
 
-combinations = Colors_Remix.create_combo(shades , colors)  # Generate all combinations
 
-end = Colors_Remix( shades['end']  ,  shades  ,  colors )  # Define end variable for resetting special formatting
+def   main(   ):
+      print(  "\n  A display of formatted text statements on screen:          \n"  )  # Example usage demonstrating how to access
+      print( f"  {drkprp}The text below is printing in normal red{reg}.       \n"  # and print different style and color mixtures
+             f"  {regred}The text above is printing in dark purple{reg}.      \n"  # using simple three-letter code abbreviations
+             f"  {drkred}This text is printing in dark red{reg}.              \n"
+             f"  {drkredbg}Now here, the text has dark red highlights{reg}.   \n"
+             f"  {drkblu}This text is printing in dark blue{reg}.             \n"
+             f"  {regblu}While the text here is printing in normal blue{reg}. \n"
+             f"  {regredbg}This is the text with normal red highlights{reg}.  \n"
+             f"  {reggre}And this text is printing with normal green{reg}.    \n"
+             f"  {drkmosbg}This text is printing in dark moss highlights{reg}.\n"
+             f"  {regprp}The text here is printing in normal purple{reg}.     \n" )
 
-print( "\n  A display of formatted text statements on screen:  \n" )  # Example usage demonstrating how to access and print different color and shade combinations directly.
-print(f"  {lgtprp}The text is printing in light purple. {end}  \n"
-      f"  {itlblu}The text is printing in italic blue.  {end}  \n"
-      f"  {itlbrw}The text is printing in italic brown. {end}  \n"
-      f"  {drkblu}The text is printing in dark blue.    {end}  \n"
-      f"  {lgtblu}The text is printing in light blue.   {end}  \n")
+      print( "\n  Demonstrating how styles and colors come together as          "
+             "\n  code to create their visual representations on screen:      \n" )
 
-print( "\n  Demonstrating how shades and colors come together as   "
-       "\n  code to provide the visual representations on screen:\n" )
-Colors_Remix.show_example( shades  ,  colors )  # Print all 18 combinations
+      Colors_Remix.show_example(   colors   ,   styles   ,   False   )  # Prints 18 combinations (no highlighting)
+      Colors_Remix.show_example(   colors   ,   styles   ,    True   )  # Prints 18 combinations (w  highlighting)
+
+
+if    __name__  ==  '__main__':
+        main(   )
